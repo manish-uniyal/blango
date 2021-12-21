66769005d8efd2fb682bc243021745b9dbe7b9bb
manish-uniyal <manish-uniyal@collegedekho.com>
2021-12-21 13:06:46 +0530

    vs new

--- blog/models.py
+++ blog/models.py
@@ -32,5 +32,7 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
+    def __str__(self):
+        return self.content 
     
     
